93f830a3fc4d28e974897b3fb25feedece29e0bd
Patrick Tierney <patrick.l.tierney@gmail.com>
2023-08-27 21:10:57 -0700

    Connected car to H-Bridges. Wheels are turning

--- mecanum_car/main.cpp
+++ mecanum_car/main.cpp
@@ -15,8 +15,28 @@ using std::endl;
 using std::vector;
 using std::map;
 
-const int WHEEL_PIN_A = 23;
-const int WHEEL_PIN_B = 24;
+// Assume where wires protrude is the front of the car
+
+// Naming Guide:
+// LF = Left Front
+// RF = Right Front
+// LB = Left Back
+// RB = Right Back
+// F = Forward
+// R = Reverse
+
+const int WHEEL_PIN_LF_F = 23;
+const int WHEEL_PIN_LF_R = 24;
+
+const int WHEEL_PIN_RF_F = 20;
+const int WHEEL_PIN_RF_R = 21;
+
+const int WHEEL_PIN_LB_F = 5;
+const int WHEEL_PIN_LB_R = 6;
+
+const int WHEEL_PIN_RB_F = 17;
+const int WHEEL_PIN_RB_R = 18;
+
 
 bool running = true;
 
@@ -47,11 +67,27 @@ int main(int argc, char** argv) {
     
     cout << "Started up GPIO" << endl;
 
-    gpioSetMode(WHEEL_PIN_A, PI_OUTPUT);
-    gpioSetMode(WHEEL_PIN_B, PI_OUTPUT);
+    gpioSetMode(WHEEL_PIN_LF_F, PI_OUTPUT);
+    gpioSetMode(WHEEL_PIN_LF_R, PI_OUTPUT);
+    
+    gpioSetMode(WHEEL_PIN_RF_F, PI_OUTPUT);
+    gpioSetMode(WHEEL_PIN_RF_R, PI_OUTPUT);
 
-    gpioPWM(WHEEL_PIN_A, 100);
-    gpioPWM(WHEEL_PIN_B, 0);
+    gpioSetMode(WHEEL_PIN_LB_F, PI_OUTPUT);
+    gpioSetMode(WHEEL_PIN_LB_R, PI_OUTPUT);
+    
+    gpioSetMode(WHEEL_PIN_RB_F, PI_OUTPUT);
+    gpioSetMode(WHEEL_PIN_RB_R, PI_OUTPUT);
+
+    gpioPWM(WHEEL_PIN_LF_F, 100);
+    gpioPWM(WHEEL_PIN_RF_F, 100);
+    gpioPWM(WHEEL_PIN_LB_F, 100);
+    gpioPWM(WHEEL_PIN_RB_F, 100);
+    
+    gpioPWM(WHEEL_PIN_LF_R, 0);
+    gpioPWM(WHEEL_PIN_RF_R, 0);
+    gpioPWM(WHEEL_PIN_LB_R, 0);
+    gpioPWM(WHEEL_PIN_RB_R, 0);
 
     while (running) {
         cout << gpioTick() << endl;
@@ -59,8 +95,14 @@ int main(int argc, char** argv) {
     }
 
     // Reset pins
-    gpioPWM(WHEEL_PIN_A, 0);
-    gpioPWM(WHEEL_PIN_B, 0);
+    gpioPWM(WHEEL_PIN_LF_F, 0);
+    gpioPWM(WHEEL_PIN_LF_R, 0);
+    gpioPWM(WHEEL_PIN_RF_F, 0);
+    gpioPWM(WHEEL_PIN_RF_R, 0);
+    gpioPWM(WHEEL_PIN_LB_F, 0);
+    gpioPWM(WHEEL_PIN_LB_R, 0);
+    gpioPWM(WHEEL_PIN_RB_F, 0);
+    gpioPWM(WHEEL_PIN_RB_R, 0);
 
     cout << "Shutting down GPIO" << endl;
     
