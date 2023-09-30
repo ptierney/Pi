a80f999ee86cb847a85b7a77adfb1f19903b89d1
Patrick Tierney <patrick.l.tierney@gmail.com>
2023-09-29 22:23:06 -0700

    Draft pass at getting GRPC server running based on hello world grpc example

--- mecanum_car/car_server/main.cpp
+++ mecanum_car/car_server/main.cpp
@@ -7,13 +7,63 @@
 #include <ostream>
 #include <vector>
 #include <map>
+#include <memory>
 
 #include <pigpio.h>
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+
+#include "mecanum.grpc.pb.h"
 
 using std::cout;
 using std::endl;
 using std::vector;
 using std::map;
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using mecanum::CarServer;
+using mecanum::MoveRequest;
+using mecanum::MoveReply;
+
+
+class CarServerServiceImpl final : public CarServer::Service {
+    Status SendMovement(ServerContext* context, const MoveRequest* request,
+                        MoveReply* reply) override {
+        // do something with the data
+
+        return Status::OK;
+    }
+
+};
+
+
+void RunServer() {
+    std::string server_address = "0.0.0.0:50051";
+
+    CarServerServiceImpl service;
+
+    grpc::EnableDefaultHealthCheckService(true);
+    grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+    ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *synchronous* service.
+    builder.RegisterService(&service);
+    // Finally assemble the server.
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
+}
 
 // Breadcrumbs / next steps:
 // * Read the gRPC C++ Tutorial: https://grpc.io/docs/languages/cpp/basics/
