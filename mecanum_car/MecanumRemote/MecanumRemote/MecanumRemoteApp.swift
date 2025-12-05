a7894379c1d5d182247e1133175af53c01fc9bef
Patrick Tierney <patrick@aro.homes>
2025-12-05 10:00:10 -0800

    Added iPad support

--- mecanum_car/MecanumRemote/MecanumRemote/MecanumRemoteApp.swift
+++ mecanum_car/MecanumRemote/MecanumRemote/MecanumRemoteApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MecanumRemoteApp: App {
     var body: some Scene {
         WindowGroup {
-            NavigationView {
+            NavigationStack {
                 DriveView()
             }
         }
